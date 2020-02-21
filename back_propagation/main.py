3e93cffcf8f77a19d1b394db9cb07d7d92dabe95
JeanCarloMachado <contato@jeancarlomachado.com.br>
2020-02-21 17:00:03 +0100

    automatic commit

--- back_propagation/main.py
+++ back_propagation/main.py
@@ -1,5 +1,6 @@
-from random import random, seed
+from random import random, seed, randrange
 from math import exp
+from csv import reader
 
 
 def initialize_network(n_inputs, n_hidden, n_outputs):
@@ -100,6 +101,93 @@ def train_network(network, train, l_rate, n_epoch, n_outputs):
 def predict(network, row):
     outputs = forward_propagate(network, row)
     return outputs.index(max(outputs))
+
+
+
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader  = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+
+    for row in dataset:
+        row[column] = lookup[row[column]]
+
+    return lookup
+
+
+def dataset_minmax(dataset):
+    stats = [[min(column), max(column)] for column in zip(*dataset)]
+
+    return stats
+
+
+def noramlize_dataset(dataset, minmax):
+    for row in dataset:
+        for i in range(len(row) -1):
+            row[i] = (row[i] - minmax[i][0] / (minmax[i][1] - minmax[i][0]))
+
+
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+
+        dataset_split.append(fold)
+        return dataset_split
+
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+
+    return correct / float(len(actual)) * 100.0
+
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *arags):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+
+        predicted = algorithm(train_set< test_set, *args)
+        actual=  [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+
+    return scores
 
 
 """
