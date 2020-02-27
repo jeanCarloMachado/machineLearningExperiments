b5989716f6ffb5a6947563ca0aafecc88fb2c4e2
JeanCarloMachado <contato@jeancarlomachado.com.br>
2020-02-27 17:00:19 +0100

    automatic commit

--- random_forest/main.py
+++ random_forest/main.py
@@ -131,3 +131,100 @@ def gini_index(groups, classes):
 def to_terminal(group):
     outcomes = [row[-1] for row in group]
     return max(set(outcomes), key=outcomes.count)
+
+
+def split(node, max_depth, min_size, n_features, depth):
+    left, right = node['groups']
+
+    del node['groups']
+
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+
+    if depth >= max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = get_split(left, n_features)
+        split(node['left'], max_depth, min_size, n_features, depth + 1)
+
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = get_split(right, n_features)
+        split(node['right'], max_depth, min_size, n_features, depth + 1)
+
+
+def build_tree(train, max_depth, min_size, n_features):
+    root = get_split(train, n_features)
+    split(root, max_depth, min_size, n_features, 1)
+
+    return root
+
+
+def predict(node, row):
+    if row[node['index']] < node['value']:
+        if isinstance(node['left'], dict):
+            return predict(node['left'], row)
+
+        return node['left']
+
+    if isinstance(node['right'], dict):
+        return predict(node['right'], row)
+
+    return node['right']
+
+
+def subsample(dataset, ratio):
+    sample = []
+    n_sample = round(len(dataset) * ratio)
+
+    while len(sample) < n_sample:
+        index = randrange(len(dataset))
+        sample.append(dataset[index])
+
+    return sample
+
+
+def bagging_predict(trees, row):
+    predictions = [predict(tree, row) for tree in trees]
+    return max(set(predictions), key=predictions.count)
+
+
+def random_forest(train, test, max_depth, min_size, sample_size, n_trees, n_features):
+    trees = list()
+
+    for _ in range(n_trees):
+        sample = subsample(train, sample_size)
+        tree = build_tree(sample, max_depth, min_size, n_features)
+
+        trees.append(tree)
+    predictions = [bagging_predict(trees, row) for row in test]
+
+    return predictions
+
+
+def test_forest():
+    seed(2)
+    filename = 'sonar.all-data.csv'
+    dataset = load_csv(filename)
+    for i in range(0, len(dataset[0]) - 1):
+        str_column_to_float(dataset, i)
+
+    str_column_to_int(dataset, len(dataset[0]) - 1)
+    n_folds = 5
+    max_depth = 10
+    min_size = 1
+    sample_size = 1.0
+
+    n_features = int(sqrt(len(dataset[0]) -1 ))
+
+    for n_trees in [1, 5, 10]: 
+        scores = evaluate_algorithm(dataset, random_forest, n_folds, max_depth, min_size, sample_size, n_trees, n_features)
+
+        print(f"Trees {n_trees}")
+        print(f"scores {scores}")
+        print(f"mean accuracy {(sum(scores)/float(len(scores)))}")
