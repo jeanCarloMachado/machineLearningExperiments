23bc17a1e0f8266c25b8bd4198a738c98290adea
JeanCarloMachado <contato@jeancarlomachado.com.br>
2020-05-04 11:27:02 +0200

    automatic commit

--- decisionTree/main.py
+++ decisionTree/main.py
@@ -1,52 +1,79 @@
 import unittest
 import grimoire as g
+import grimoire.test as t
 import streamlit as st
+
 """
 # Gini index
 
-Gives an idea of how good the split is by how mixed the classes are in the two groups created by the split.
+The goal is to have perfect separatio between groups.
+So one class is all in one group and the other is all in the other group.
 
-A perfect separation results in gini score of 0
+This perfect scenario the return value is zero.
+It keeps increasing until the worse case scenario 0.5
 
 """
 
 
-def gini_index(groups, classes):
-    n_instances = sum([len(group) for group in groups])
+def gini_index(groups, classes=[0,1]):
+
+    total_population = g.reduce(lambda y, x: len(x) + y, groups)
 
     gini = 0
 
     for group in groups:
-        size = len(group)
+        group_population = len(group)
 
-        if size == 0:
+        if group_population == 0:
             continue
 
-        score = 0
+        group_score = 0
         for class_val in classes:
-            p = [row[-1] for row in group].count(class_val) / size
-            score += p * p
+            percentage_in_group_with_class = group.count(class_val) / group_population
+            group_score += percentage_in_group_with_class**2
 
-        gini += (1.0 - score) * (size / n_instances)
+        percentage_of_total_population = group_population / total_population
+
+        gini += (1.0 - group_score) * percentage_of_total_population
 
     return gini
 
 
 class GiniTest(unittest.TestCase):
-    def test_worst_split_50_each_side(self):
-        groups = [
-            [[1, 1], [1, 0]],
-            [[1, 1], [1, 0]]
-        ]
-        self.assertEqual(0.5, gini_index(groups, [0, 1]))
-
     def test_perfect_separation(self):
         groups = [
-            [[1, 0], [1, 0]],
-            [[1, 1], [1, 1]]
+            [0, 0],
+            [1, 1]
         ]
-        self.assertEqual(0.0, gini_index(groups, [0, 1]))
+        self.assertEqual(0.0, gini_index(groups))
+
+    def test_unbalanced_groups(self):
+        groups = [
+            [0, 0],
+            []
+        ]
+        self.assertEqual(0.0, gini_index(groups))
+
+    def test_medium_case(self):
+        groups = [
+            [1, 1, 0],
+            [1, 0, 0]
+        ]
+        self.assertEqual(0.44, round(gini_index(groups), 2))
+
+        groups = [
+            [1, 1, 1, 0],
+            [1, 0, 0, 0]
+        ]
+        self.assertEqual(0.38, round(gini_index(groups), 2))
 
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_worst_split_50_each_side(self):
+        groups = [
+            [1, 0],
+            [1, 0]
+        ]
+        self.assertEqual(0.5, gini_index(groups))
+
+#t.run_test_case(GiniTest)
+
