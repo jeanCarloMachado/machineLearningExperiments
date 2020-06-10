445e0e0e419b3e11dc3d7dfa3758fb38fa876847
JeanCarloMachado <contato@jeancarlomachado.com.br>
2020-06-10 17:00:12 +0200

    automatic commit

--- titanic/main.py
+++ titanic/main.py
@@ -6,13 +6,25 @@ import grimoire.time as t
 """
 # Titanic challenge
 
-## Steps
- - Clean the data
-    Encode other features to fit the model
-"""
+## Next steps I can take
+
+-  Hyper parameter tunning
+-  Hotencoding features
+-  Estimate prediction variatio from previous one.
+-  Register estimation values.
 
 
-"""
+### Log (newest on top):
+
+- estimators 200 - 0.74641
+- estimators 30 - 0.73205
+- remove siblings again - 0.74162
+- 70 estimators - 0.71770
+- 100 estimaors - 0.71291
+- new attribues (class, siblings) -  0.72727
+- added sex to features - 0.74641
+
+
 ## Understanding the training data
 
 
@@ -20,48 +32,57 @@ import grimoire.time as t
 train = pd.read_csv('train.csv')
 with st.echo():
     st.write(train.sample(n=5))
-    st.write(train.describe())
-
-train = train.fillna(method='ffill')
 
 """
 Website [with description](https://www.kaggle.com/c/titanic/data?select=test.csv)
 
-- Not everybody has an age defined
 
 
-Variable	Definition	Key
-survival	Survival	0 = No, 1 = Yes
-pclass	Ticket class	1 = 1st, 2 = 2nd, 3 = 3rd
-sex	Sex	
-Age	Age in years	
-sibsp	# of siblings / spouses aboard the Titanic	
-parch	# of parents / children aboard the Titanic	
-ticket	Ticket number	
-fare	Passenger fare	
-cabin	Cabin number	
-embarked	Port of Embarkation	C = Cherbourg, Q = Queenstown, S = Southampton
+Variable | Definition |	Key
+- survival	Survival	0 = No, 1 = Yes
+- pclass	Ticket class	1 = 1st, 2 = 2nd, 3 = 3rd
+- sex	Sex	
+- Age	Age in years	
+- sibsp	# of siblings / spouses aboard the Titanic	
+- parch	# of parents / children aboard the Titanic	
+- ticket	Ticket number	
+- fare	Passenger fare	
+- cabin	Cabin number	
+- embarked	Port of Embarkation	C = Cherbourg, Q = Queenstown, S = Southampton
 
-
- - What is parch? Number of parents
 
 parch: The dataset defines family relations in this way...
 Parent = mother, father
 Child = daughter, son, stepdaughter, stepson
 Some children travelled only with a nanny, therefore parch=0 for them.
 
+- Not everybody has an age defined
+
+"""
+with st.echo():
+    st.write(train.describe())
+
+"""
 ---
 
 
+
 ## Prepare the data
+
+
+
 """
+
+train = train.fillna(method='ffill')
+
+
 from sklearn.ensemble import RandomForestClassifier
 
 y = train['Survived']
 
 with st.echo():
     def cleanup_df(df):
-        ignored_cols = ['Survived', 'Cabin', 'Embarked', 'Name', 'PassengerId', 'PClass', 'SibSp', 'Ticket']
+        ignored_cols = ['Survived', 'Cabin', 'Embarked', 'Name', 'PassengerId', 'Ticket', 'SibSp']
         df = df[df.columns.difference(ignored_cols)]
         df['Sex'] = df['Sex'].map(lambda x: 0 if x == 'male' else 1)
         return df.fillna(method='ffill')
@@ -77,7 +98,7 @@ X
 ### Fit the data
 """
 with st.echo():
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(n_estimators=200)
     clf.fit(X, y)
 
 
